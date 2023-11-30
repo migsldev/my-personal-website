8b820c9a1896999c315682f98129e81432951982
Miguel Leonar <migsldev@gmail.com>
2023-11-30 18:40:01 +1100

    finish website, removed comments and other files

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // creating a basic guided function for the hamburger menu/icon
 
 function toggleMenu() { // targeting elements (.menu-links and .hamburger-icon) on the webpage and basically using that element
-    const menu = document.querySelector(".menu-links"); // using cons instead of var/let as the function name as it cannot be updated or re-declared
+    const menu = document.querySelector(".menu-links"); 
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open"); // when clicked, this function will add or remove the "open" class. this class will have some styling - style.css              
+    menu.classList.toggle("open"); // when clicked, this function will add or remove the "open" class.            
     icon.classList.toggle("open");                       
 }
